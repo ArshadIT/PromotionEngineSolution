dd72ad3502490ed49c6d2ee01098b7ef82e3032d
ArshadIT <arshadecon@gmail.com>
2021-09-06 10:40:01 +0200

    scenario 3 testing

--- PromotionEngine.Unit.Tests/PromotionEngineTests.cs
+++ PromotionEngine.Unit.Tests/PromotionEngineTests.cs
@@ -49,5 +49,26 @@ namespace PromotionEngine.Unit.Tests
 
             Assert.True(actual == total);
         }
+
+        [Fact]
+        public void Test3_WithPromotionsCAndDSku()
+        {
+
+            List<IPromotion> promotions = new List<IPromotion>();
+            promotions.Add(new SkuCAndDPromotion()); //30
+
+            //Arrange 
+            Cart cart = new Cart(promotions);
+
+            cart.SKUs.Add(new SkuA());  //50
+            cart.SKUs.Add(new SkuB());  //30
+            cart.SKUs.Add(new SkuC());  //20
+            cart.SKUs.Add(new SkuD());  //15
+
+            decimal actual = 50 + 30 + 20 + 15 + 30;
+            var total = cart.GetTotalPriceAfterPromotion();
+
+            Assert.True(actual == total);
+        }
     }
 }
